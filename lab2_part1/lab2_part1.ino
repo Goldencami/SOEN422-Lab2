4d09c68fecf09bc1ab0a6ee4d36fbb76baa32409
Camila Guzman <camila.guzman023@gmail.com>
2025-10-05 15:55:37 -0400

    created struct for songs and updated the return type of functions to use them in play()

--- lab2_part1/lab2_part1.ino
+++ lab2_part1/lab2_part1.ino
@@ -16,8 +16,46 @@ String STUDENT_ID = "40239038";
 String DEVICE1_NAME = "Camila_iPhone16";
 String DEVICE2_NAME = "Camila_MBP";
 
-JSONVar httpGET(String endpoint) { // return string??
-  if (WiFi.status() != WL_CONNECTED) return JSONVar();
+struct Song {
+  String name = "undefined";
+  int tempo;
+  int melody[50];
+  int length = 0;
+};
+
+void play(Song object) {
+  int notes = sizeof(object.melody) / sizeof(object.melody[0]) / 2;
+  int wholenote = (60000 * 4) / object.tempo;
+  int divider = 0, noteDuration = 0;
+
+  // iterate over the notes of the melody.
+  // Remember, the array is twice the number of notes (notes + durations)
+  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+    // calculates the duration of each note
+    divider = object.melody[thisNote + 1];
+    if (divider > 0) {
+      // regular note, just proceed
+      noteDuration = (wholenote) / divider;
+    } else if (divider < 0) {
+      // dotted notes are represented with negative durations!!
+      noteDuration = (wholenote) / abs(divider);
+      noteDuration *= 1.5; // increases the duration in half for dotted notes
+    }
+
+    // we only play the note for 90% of the duration, leaving 10% as a pause
+    tone(BUZZER_PIN, object.melody[thisNote], noteDuration * 0.9);
+
+    // Wait for the specief duration before playing the next note.
+    delay(noteDuration);
+
+    // stop the waveform generation before the next note.
+    noTone(BUZZER_PIN);
+  }
+}
+
+Song httpGET(String endpoint) {
+  Song randomSong;
+  if (WiFi.status() != WL_CONNECTED) return randomSong;
 
   HTTPClient http;
   http.begin(BASE_URL + endpoint);
@@ -27,73 +65,70 @@ JSONVar httpGET(String endpoint) { // return string??
 
   if (httpResponseCode > 0) {
     Serial.println("HTTP GET Response: " + String(httpResponseCode));
-    Serial.println(http.getString()); // prints payload
+    payload = http.getString();
+    Serial.println(payload);
+  }
+  else {
+    Serial.println("HTTP GET Error: " + String(httpResponseCode));
+    return randomSong;
+  }
+
+  http.end(); // Free resources
+
+  JSONVar songObj = JSON.parse(payload);
+  if (JSON.typeof(songObj) == "undefined" || !songObj.hasOwnProperty("melody")) {
+    Serial.println("Parsing input failed!");
+    return randomSong;
+  }
+
+  randomSong.name = (const char*) songObj["name"]; // converts JSON string into a string C++ can accept
+  randomSong.tempo = atoi((const char*) songObj["tempo"]); // converts string to an integer
+
+  JSONVar melodyArr = songObj["melody"];
+  randomSong.length = songObj["melody"].length();
+
+  for (int i = 0; i < randomSong.length; i++) {
+    randomSong.melody[i] = atoi((const char*) melodyArr[i]);
+  }
+
+  return randomSong;
+}
+
+Song getSong(String song_name) {
+  return httpGET("/song?name=" + song_name);
+}
+
+Song getPreferedSong(String student_id, String device) {
+  Song emptySong;
+  if (WiFi.status() != WL_CONNECTED) return emptySong;
+
+  HTTPClient http;
+  http.begin(BASE_URL + "/preference?id=" + student_id + "&key=" + device);
+
+  int httpResponseCode = http.GET(); // will only return the name of the prefered song of the linked device
+  String payload = "";
+
+  if (httpResponseCode > 0) {
     payload = http.getString();
   }
   else {
     Serial.println("HTTP GET Error: " + String(httpResponseCode));
+    return emptySong;
   }
-  
   http.end(); // Free resources
-  // return payload;
-
-  if (payload == "") return JSONVar();
 
   JSONVar songObj = JSON.parse(payload);
-
   if (JSON.typeof(songObj) == "undefined") {
     Serial.println("Parsing input failed!");
-    return JSONVar();
+    return emptySong;
   }
 
-  return songObj;
-}
-
-JSONVar getSong(String song_name) { // change to string??
+  String song_name = (const char*) songObj["name"]; // converts JSON string into a string C++ can accept
   return httpGET("/song?name=" + song_name);
-  // if (payload == "") return JSONVar();
- 
-  // JSONVar songObj = JSON.parse(payload);
-
-  // if (JSON.typeof(songObj) == "undefined") {
-  //   Serial.println("Parsing input failed!");
-  //   return JSONVar();
-  // }
-
-  // return songObj;
-
-
-  // int tempo = (int)songObj["tempo"];
-  // JSONVar melody = songObj["melody"];
-
-  // Serial.println("Tempo: " + String(tempo));
-  // Serial.print("Melody: ");
-
-  // for (int i = 0; i < melody.length(); i++) {
-  //   Serial.print((int)melody[i]);
-  //   Serial.print(" ");
-  // }
-  // Serial.println();
-  
-  // return payload;
-}
-
-JSONVar getPreferenceSong(String student_id, String device) {
-  return httpGET("/preference?id=" + student_id + "&key=" + device);
-  // if (payload == "") return JSONVar();
-
-  // JSONVar songObj = JSON.parse(payload);
-
-  // if (JSON.typeof(songObj) == "undefined") {
-  //   Serial.println("Parsing input failed!");
-  //   return JSONVar();
-  // }
-
-  // return songObj;
 }
 
 void postDevice(String student_id, String device, String song_name) {
-  if (WiFi.status() != WL_CONNECTED) return;
+  if (WiFi.status() != WL_CONNECTED) return ;
 
   String endpoint = "/preference?id=" + student_id + "&key=" + device + "&value=" + song_name;
 
@@ -103,17 +138,16 @@ void postDevice(String student_id, String device, String song_name) {
 
   if (httpResponseCode > 0) {
     Serial.println("HTTP POST Response: " + String(httpResponseCode));
-    Serial.println(http.getString()); // prints payload
   }
   else {
     Serial.println("HTTP POST Error: " + String(httpResponseCode));
+    return;
   }
   
   http.end(); // Free resources
 }
 
-void setup()
-{
+void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   WiFi.begin(ssid, password);
@@ -132,12 +166,13 @@ void setup()
 
   Serial.println("GET RANDOM SONG");
   httpGET("/song");
-  // Serial.println("POST DEVICE");
-  // postDevice(STUDENT_ID, DEVICE1_NAME, "harrypotter");
+  Serial.println("POST DEVICE");
+  postDevice(STUDENT_ID, DEVICE1_NAME, "harrypotter");
   Serial.println("GET DEVICE");
-  getPreferenceSong(STUDENT_ID, DEVICE1_NAME);
+  Song toPlay = getPreferedSong(STUDENT_ID, DEVICE1_NAME);
   Serial.println("GET SONG");
   getSong("gameofthrones");
+  play(toPlay);
 }
 
 void loop() {
